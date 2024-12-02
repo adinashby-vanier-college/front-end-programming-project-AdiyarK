effafbbd4a4345702977a3a059c244474e06ecd5
Adiyar Kaidaruly <2219687@crosemont.qc.ca>
2024-12-02 15:21:50 -0500

    js typing animation

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const word = "Web Developer";
+let timer;
+
+function typingEffect() {
+    let summary = word.split('');
+    var loopTyping = function () {
+        if (summary.length > 0) {
+            document.getElementById('summary').innerHTML += summary.shift();
+        } else {
+            deletingEffect();
+            return false;
+        };
+        timer = setTimeout(loopTyping, 100);
+    };
+    loopTyping();
+};
+
+
+function deletingEffect(){
+    let summary = word.split('');
+    var loopDeleting = function () {
+        if (summary.length > 0) {
+            summary.pop()
+            document.getElementById('summary').innerHTML = summary.join("");
+        } else {
+            typingEffect();
+            return false;
+        };
+        timer = setTimeout(loopDeleting, 100);
+    };
+    loopDeleting();
+};
+typingEffect();
